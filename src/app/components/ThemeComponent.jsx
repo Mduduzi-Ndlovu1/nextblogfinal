0c284715a5e3441ac9021fba08f523917f5710ee
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-12-19 21:33:39 +0200

    Feat: - Add Next-theme package - Add ThemeProvider and ThemeComponent - Add onClick function for light and dark mode -Add conditional rendering for toggleTheme icon

---
+++ src/app/components/ThemeComponent.jsx
@@ -0,0 +1,23 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+
+export default function ThemeComponent({children}) {
+    const {theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+  if (!mounted) {
+    return null
+  }
+  return (
+    <div className={theme}>
+        <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen'>
+            {children}
+        </div>
+    </div>
+  )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { ThemeProvider } from "next-themes";
+import ThemeComponent from "./components/ThemeComponent";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,13 +22,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
-        <Footer/>
+        <ThemeProvider>
+          <ThemeComponent>
+            <Header />
+            {children}
+            <Footer/>
+          </ThemeComponent>
+        </ThemeProvider>
       </body>
     </html>
   );
