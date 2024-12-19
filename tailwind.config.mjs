b8ab35425ca9265ddd87072c4613971f4755f2ad
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-12-19 21:15:27 +0200

    Feat -Create the header section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,15 @@
+const flowbite = require("flowbite-react/tailwind");
+
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/**/*.js", // Add this for Flowbite.
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -13,5 +19,8 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+  
 };
