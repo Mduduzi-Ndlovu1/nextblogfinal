0fdcf8a06ddc1774bf2156b2517621b203db8688
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-12-27 23:15:56 +0200

    fix: Post function didnt have a req

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 
-export async function POST() {
+export async function POST(req) {
   const SIGNING_SECRET = process.env.WEBHOOK_SECRET
 
   if (!SIGNING_SECRET) {
