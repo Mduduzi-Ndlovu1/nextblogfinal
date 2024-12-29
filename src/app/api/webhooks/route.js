afa473fe38c54e9c877520ec414d0308adb93d40
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-12-29 12:53:07 +0200

    Feat: -add mongoDB -save users from clerk

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,5 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
+import { createUserOrUpdateUser, deleteUser } from '@/app/lib/actions/user'
+import { clerkClient } from '@clerk/nextjs/dist/types/server'
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.WEBHOOK_SECRET
@@ -46,17 +48,65 @@ export async function POST(req) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data
-  const eventType = evt.type
+  const { id } = evt?.data
+  const eventType = evt?.type
   console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
   console.log('Webhook payload:', body)
 
-  if (evt.type === 'user.created') {
-    console.log('userId:', evt.data.id)
+  if(eventType === 'user.created' || eventType === 'user.updated'){
+    const {
+      id,
+      first_name,
+      last_name,
+      image_url,
+      email_addresses,
+      username
+    } = evt?.data;
+
+    try {
+      const user = await createUserOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username,
+      )
+
+      if(user && eventType === 'user.created'){
+        try {
+          await clerkClient.users.updateUserMetaData(id, {
+            publicMetadata: {
+              userMongoId: user._id,
+              isAdmin: user.isAdmin,
+            }
+          })
+        } catch (error) {
+          console.log('====================================');
+          console.log('Error updating user metaData:', error);
+          console.log('====================================');
+        }
+      }
+    } catch (error) {
+      console.log('====================================');
+      console.log('Error updating user:', error);
+      console.log('====================================');
+      return new Response('Error occured', {status:400})
+      
+    }
   }
 
-  if (evt.type === 'user.updated') {
-    console.log('user is updated:', evt.data.id)
-  }
+  if(eventType === 'user.deleted'){
+    const {id} = evt?.data;
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log('====================================');
+      console.log('Error Deleting user:', error);
+      console.log('====================================');
+      return new Response('Error occured', {status:400})
+    }
+  };
+  
   return new Response('Webhook received', { status: 200 })
 }
---
+++ src/app/lib/mongodb/mongoose.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let initialized = false;
+
+export const connect = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(initialized) {
+        console.log("Already connected to MongoDB");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName:'nextblogproject',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+
+        });
+        initialized = true;
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error);
+    }
+};
