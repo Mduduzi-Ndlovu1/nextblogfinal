5d594911d691ce35996e1f3031a119e6c323896c
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-01-07 17:29:59 +0200

    final

--- src/app/page.js
+++ src/app/page.js
@@ -18,27 +18,31 @@ export default async function Home() {
   return (
     <div className='flex flex-col justify-center items-center'>
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
-        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
-        <p className='text-gray-500 text-sm sm:text-base'>
-          Discover a variety of articles and tutorials on topics such as web
-          development, software engineering, and programming languages, all
-          brought to you through a blog built with Next.js and{' '}
-          <a
-            href='https://go.clerk.com/fgJHKlt'
-            className='text-teal-500 hover:underline'
-            target='_blank'
-          >
-            Clerk
-          </a>
-          .
-        </p>
-        <Link
-          href='/search'
+  <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to My Tech Journey</h1>
+  <p className='text-gray-500 text-sm sm:text-base'>
+    Dive into the world of technology through the eyes of a junior developer! 
+    This blog is your go-to place for relatable insights, beginner-friendly 
+    tutorials, and personal experiences as I navigate the exciting challenges 
+    of web development, software engineering, and programming. Built with 
+    Next.js and powered by{' '}
+    <a
+      href='https://go.clerk.com/fgJHKlt'
+      className='text-teal-500 hover:underline'
+      target='_blank'
+    >
+      Clerk
+    </a>, 
+    this space is where learning meets storytelling, perfect for developers 
+    just starting their tech adventure.
+  </p>
+  <Link
+          href={'/search?category=null'}
           className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
-        >
-          View all posts
-        </Link>
-      </div>
+  >
+    Explore all posts
+  </Link>
+</div>
+
       <div className='p-3 bg-amber-100 dark:bg-slate-700'>
         <CallToAction />
       </div>
